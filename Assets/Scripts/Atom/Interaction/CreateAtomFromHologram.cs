6665af3f9a5402c9ffaa0ae187283f39fb193eea
FrankBroeks1996 <31690726+FrankBroeks1996@users.noreply.github.com>
2020-01-22 14:55:00 +0100

    Create atom from hologram (#21)
    * Added create atom from hologram
    
    * Update create atom from hologram
    
    * Update to use trigger instead of grip
    
    * Update lab prefab
    
    * Update .gitignore
    
    * Changed create atom so it uses the spawnpoint
    
    * Update AtomButton.cs
    
    * Fix assignment in lab prefab
    
    Co-authored-by: Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>

---
+++ Assets/Scripts/Atom/Interaction/CreateAtomFromHologram.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Atom.Builder;
+using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Player;
+using UnityEngine;
+
+public class CreateAtomFromHologram : MonoBehaviour, IMixedRealityInputHandler
+{
+    [Header("Input")]
+    [SerializeField] private MixedRealityInputAction action = MixedRealityInputAction.None;
+
+    [SerializeField] private AtomBuilder atomBuilder = null;
+    
+    private bool grabbableControllerInTrigger = false;
+
+    private void Awake()
+    {
+        CoreServices.InputSystem?.RegisterHandler<IMixedRealityInputHandler>(this);
+    }
+
+    public void OnInputDown(InputEventData eventData)
+    {
+        if (eventData.MixedRealityInputAction == action && grabbableControllerInTrigger) atomBuilder.CreateAtom();
+    }
+
+    public void OnInputUp(InputEventData eventData) { }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<GrabbableController>()) grabbableControllerInTrigger = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<GrabbableController>()) grabbableControllerInTrigger = false;
+    }
+}
