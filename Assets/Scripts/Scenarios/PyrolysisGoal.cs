3ea9fdbf7f46ad8e4d4754d5039c896f002e3611
Tom de Wildt <32359630+tomdewildt@users.noreply.github.com>
2020-01-22 11:54:55 +0100

    Add pyrolysis scenario (#13)
    * Add pyrolysis scenario
    
    * Add bottles to pyrolysis scene

---
+++ Assets/Scripts/Scenarios/PyrolysisGoal.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+using Environment.Props.Pyrolysis;
+using Utilities;
+
+namespace Scenarios
+{
+    public class PyrolysisGoal : MonoBehaviour
+    {
+        [Header("Environment")]
+        [SerializeField] private GameObject door = null;
+
+        [Header("Scene")]
+        [SerializeField] private SceneLoader loader = null;
+        [SerializeField] private UnityScene scene = null;
+        [SerializeField] private float timeout = 5.0f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            PyrolysisJerrycan jerrycan = other.GetComponent<PyrolysisJerrycan>();
+            if (jerrycan != null && jerrycan.AmountOfFuel > 0)
+            {
+                Destroy(door);
+                loader.LoadScene(scene, timeout);
+            }
+        }
+    }
+}
