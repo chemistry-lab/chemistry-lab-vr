dd6fa2690bd725f16abece5c3d9d45f920136b95
SvanSomeren <31691102+SvanSomeren@users.noreply.github.com>
2020-01-28 12:02:05 +0100

    Add molecule machine reset button (#71)

---
+++ Assets/Scripts/Molecule/Interaction/MoleculeResetButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+
+using Interactables;
+using Molecule.Builder;
+
+namespace Molecule.Interaction
+{
+    [RequireComponent(typeof(Collider))]
+    public class MoleculeResetButton : MonoBehaviour
+    {
+        [Header("Build")]
+        [SerializeField] private MoleculeBuilder builder = null;
+
+        [Header("Highlight")]
+        [SerializeField] private Renderer button = null;
+        [Range(0f, 0.1f)]
+        [SerializeField] private float thickness = 0.025f;
+
+        [Header("Audio")] 
+        [SerializeField] private AudioSource audioSource = null;
+        [SerializeField] private AudioClip audioClip = null;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            builder.Reset();
+            button.material.SetFloat("_OutlineWidth", thickness);
+            audioSource.PlayOneShot(audioClip);
+        }
+
+        private void OnTriggerExit(Collider other)
+        {
+            button.material.SetFloat("_OutlineWidth", 0f);
+        }
+    }
+}
